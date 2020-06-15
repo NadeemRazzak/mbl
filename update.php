61448989b756391e8db4b4fe1179b74310665009
NadeemRazzak <61326871+NadeemRazzak@users.noreply.github.com>
2020-06-15 14:57:30 +0500

    Add files via upload

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-$myObj->versionCode = "1.18";
+$myObj->versionCode = "1.19";
 $myObj->features = "Update";
 $myObj->url = "https://github.com/NadeemRazzak/mbl/blob/master/MBL_Verisys_1.17_final.apk";
 
