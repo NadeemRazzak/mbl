24fd269e673afd56ab3cc52ce771089edb0daa99
NadeemRazzak <61326871+NadeemRazzak@users.noreply.github.com>
2020-06-17 15:21:23 +0500

    Delete update.php

--- update.php
+++
@@ -1,8 +0,0 @@
-<?php
-$obj = new stdClass();
-$obj->versionCode = "1.19";
-$obj->features = "Update";
-$obj->url = "https://github.com/NadeemRazzak/mbl/raw/master/MBL_Verisys_1.17_final.apk";
-
-echo json_encode($obj);
-?>
